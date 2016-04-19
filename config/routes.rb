522895a9903dd1a5861f624da66a8ac1fa905ac9
Firmin Saint-Amour <firminsa@gmail.com>
2016-04-19 12:43:06 -0400

    Stripe
    Implemented stripe, this will fully implemented when the menu,
    menu_items, and shopping car are implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resource :charges
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   get '/users/auth/twitter/callback', to: 'omniauth_callbacks#twitter'
   root 'home#home'
